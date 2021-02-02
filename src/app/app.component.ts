ebb1bf11d1d1db42800fdcb2f08da2f3b39b64dc
Zack Wilcox <zwilcox96@gmail.com>
2021-02-01 17:33:15 -0800

    No more Alex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AlexStatus';
+  title = 'HatStatus';
   partyTime: boolean;
 
   onPartyTime(isParty: boolean): void {
